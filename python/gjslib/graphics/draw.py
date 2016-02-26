ce1ccf4e5bd0b3f2ad3f9c9e78e9e77d6505decd
Gavin Stark <nfp@winslowstark.org>
2016-02-26 09:15:29 +0000

    [draw] Added in fill paths function

--- python/gjslib/graphics/draw.py
+++ python/gjslib/graphics/draw.py
@@ -13,7 +13,18 @@ class c_line(object):
             self.tl = (l[2],l[3])
             self.br = (l[0],l[1])
             pass
+        self.dx = l[2]-l[0]
+        self.dy = l[3]-l[1]
         pass
+    #f x_at_y
+    def x_at_y(self, y):
+        if self.dy==0: return 0
+        #print y, y-self.tl[1], ((y-self.tl[1]) * self.dx) / self.dy
+        return ((y-self.tl[1]) * self.dx) / self.dy + self.tl[0]
+    #f __repr__
+    def __repr__(self):
+        r = "( (%d,%d), (%d,%d) )"%(self.tl[0],self.tl[1], self.br[0],self.br[1])
+        return r
     pass
 
 #c c_draw_buffer
@@ -98,6 +109,82 @@ class c_draw_buffer(object):
                 pass
             pass
         pass
+    #f fill_paths
+    def fill_paths(self, paths, resolution_bits=16, value=255, winding_rule=0):
+        """
+        paths is a set of lists of points
+        Each path is implicitly closed
+
+        The algorithm is to generate a set of lines from the paths
+        Sort the lines so lowest top-left y is first; this is the 'ready list'
+        Start with an empty 'active lines' set
+        For each y pixel line (can start at y=pixel line of first in ready list)
+        1. determine if the first of the ready list should be activated; if so, activate
+        and move on to the next in the ready list
+        2. for all the lines in the active set determine if the line:
+        2a. is now inactive (i.e. pixel y >= bottom y), in which case deactivate
+        2b. crosses at pixel y - and return its X coordinate
+        Note that horizontal lines on a pixel y immediately deactivate
+        Now sort the returned X coordinates, and draw rows of pixels according to the winding rule at pixel Y
+        Move down a line
+        Continue until ready list and active set are all empty
+        """
+        d = 1<<resolution_bits
+        ready_list = []
+        for p in paths:
+            if len(p)<2: continue
+            last_pt = (int(p[-1][0]*d), int(p[-1][1]*d))
+            for pt in p:
+                pt = (int(pt[0]*d), int(pt[1]*d))
+                ready_list.append( c_line((pt[0],pt[1],last_pt[0],last_pt[1])) )
+                last_pt = pt
+                pass
+            pass
+        if len(ready_list)<1:
+            return
+        def sort_lines(l0,l1):
+            if (l0.tl[1]<=l1.tl[1]): return -1
+            return 1
+        ready_list.sort(cmp=sort_lines)
+        active_set = set()
+        py = (ready_list[0].tl[1])>>resolution_bits
+        y = py << resolution_bits
+        while (len(ready_list)>0) or (len(active_set)>0):
+            while len(ready_list)>0:
+                if ready_list[0].tl[1] > y:
+                    break
+                active_set.add(ready_list.pop(0))
+                pass
+            lines_complete = []
+            x_crossings = []
+            for l in active_set:
+                if y >= l.br[1]:
+                    lines_complete.append(l)
+                    pass
+                else:
+                    x_crossings.append(l.x_at_y(y))
+                    pass
+                pass
+            for l in lines_complete:
+                active_set.remove(l)
+                pass
+            x_crossings.sort()
+            rpx = None
+            for i in range(len(x_crossings)):
+                lpx = rpx
+                rpx = x_crossings[i] >> resolution_bits
+                if (i%2)==1:
+                    for x in range(rpx-lpx):
+                        self.set_pixel(lpx+x,py,value)
+                        pass
+                    pass
+                pass
+
+            #print py, x_crossings
+            py += 1
+            y += 1<<resolution_bits
+            pass
+        pass
     #f __repr__
     def __repr__(self):
         r = "draw_buffer\n"
@@ -125,18 +212,23 @@ class c_draw_buffer(object):
 if __name__=="__main__":
     d = c_draw_buffer(size=(80,80),bytes_per_pixel=1)
     d.set_pixel(16,16)
-    d.draw_line( (0,0,80,0), value=64 )
-    d.draw_line( (0,0,0,80), value=64 )
-    d.draw_line( (79,0,79,79), value=64 )
-    d.draw_line( (79,79,0,79), value=64 )
-    d.draw_line( (40,40,60,35) )
-    d.draw_line( (40,40,60,45) )
-    d.draw_line( (40,40,20,35) )
-    d.draw_line( (40,40,20,45) )
-    d.draw_line( (40,40,45,20) )
-    d.draw_line( (40,40,35,20) )
-    d.draw_line( (40,40,45,60) )
-    d.draw_line( (40,40,35,60) )
+    d.fill_paths( [((10,10), (70,10), (70,70), (10,70)),
+                   ((40,10), (10,40), (40,70), (70,40)),
+                   ],value=64 )
+    if False:
+        d.draw_line( (0,0,80,0), value=64 )
+        d.draw_line( (0,0,0,80), value=64 )
+        d.draw_line( (79,0,79,79), value=64 )
+        d.draw_line( (79,79,0,79), value=64 )
+        d.draw_line( (40,40,60,35) )
+        d.draw_line( (40,40,60,45) )
+        d.draw_line( (40,40,20,35) )
+        d.draw_line( (40,40,20,45) )
+        d.draw_line( (40,40,45,20) )
+        d.draw_line( (40,40,35,20) )
+        d.draw_line( (40,40,45,60) )
+        d.draw_line( (40,40,35,60) )
+        pass
     print d
 
 
